bf52f31d3b2fa770cb5d85611a7804be0fe0865d
Romeo Bellon <romeo.bellon@me.com>
2020-04-07 22:42:06 +0800

    Update ngx-bootstrap

--- projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.module.ts
+++ projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.module.ts
@@ -1,26 +1,27 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { NgxIntlTelInputComponent } from './ngx-intl-tel-input.component';
-import { CommonModule } from '@angular/common';
-import { BsDropdownModule, TooltipModule } from 'ngx-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgxIntlTelInputService } from './ngx-intl-tel-input.service';
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {NgxIntlTelInputComponent} from './ngx-intl-tel-input.component';
+import {CommonModule} from '@angular/common';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgxIntlTelInputService} from './ngx-intl-tel-input.service';
 
 @NgModule({
-	declarations: [NgxIntlTelInputComponent],
-	imports: [
-		CommonModule,
-		FormsModule,
-		ReactiveFormsModule,
-		BsDropdownModule.forRoot(),
-		TooltipModule.forRoot()
-	],
-	exports: [NgxIntlTelInputComponent]
+  declarations: [NgxIntlTelInputComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot()
+  ],
+  exports: [NgxIntlTelInputComponent]
 })
 export class NgxIntlTelInputModule {
-	static forRoot(): ModuleWithProviders {
-		return {
-			ngModule: NgxIntlTelInputModule,
-			providers: [NgxIntlTelInputService]
-		};
-	}
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: NgxIntlTelInputModule,
+      providers: [NgxIntlTelInputService]
+    };
+  }
 }
